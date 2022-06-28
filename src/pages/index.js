e3443a43ca7f3d0238c96061cbb97302ed0d5478
seonghee-lee <heeracle@naver.com>
2022-06-28 11:31:53 +0900

    GND 만들기 , Head컴포넌트 만들어서 중복 부분 제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Head from "../components/Head";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+
+      <Head />
+
       <h1>Home Page</h1>
     </main>
   );
